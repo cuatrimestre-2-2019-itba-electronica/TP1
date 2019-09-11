25bddce55cb4b93c651cc19d7e939481edd87649
El-Arthur <54154342+El-Arthur@users.noreply.github.com>
2019-09-11 14:41:33 -0300

    Encoder driver anda

--- encoder.c
+++ encoder.c
@@ -1,23 +1,90 @@
 #include "encoder.h"
+#include "SysTick.h"
+
+static int aState;
+static int aLastState;
+
+static int cState;
+static int cLastState;
+
+void doNothing(){}
 
 int encoder_init(){
 	counter = 0;
-	//callback_CW = NULL;
-	//callback_CCW = NULL;
-	//callback_SW = NULL;
+	callback_CW = doNothing;
+	callback_CCW = doNothing;
+	callback_SW_PRESS = doNothing;
+	callback_SW_RELEASE = doNothing;
 
-	gpioMode(PIN_ENCODER_A, INPUT);
-	gpioMode(PIN_ENCODER_B, INPUT);
-	gpioMode(PIN_ENCODER_C, INPUT);
-	
+	MYgpioMode(PIN_ENCODER_A, INPUT);
+	MYgpioMode(PIN_ENCODER_B, INPUT);
+	MYgpioMode(PIN_ENCODER_C, INPUT);
 
-	if(!gpioIRQ(PIN_ENCODER_A, GPIO_IRQ_MODE_RISING_EDGE, encoder_action))
-		return 0;
-	if(!gpioIRQ(PIN_ENCODER_C, GPIO_IRQ_MODE_FALLING_EDGE, encoder_action_SW))
-		return 0;
+	cLastState = MYgpioRead(PIN_ENCODER_C);
+	aLastState = MYgpioRead(PIN_ENCODER_A);
+
+	SysTick_Init(encoder_read);
 
 	return 1;
 }
+
+void encoder_read(){
+	encoder_read_POS();
+	encoder_read_SW();
+}
+
+/*******************************************************************************
+ * ENCODER FUNCTIONS
+ ******************************************************************************/
+
+void encoder_action(){
+
+	// If B is HIGH -> CW
+	if(MYgpioRead(PIN_ENCODER_B) == HIGH){
+		counter ++;
+		callback_CW();
+	}
+	// If not -> CCW
+	else{
+		counter --;
+		callback_CCW();
+	}
+}
+
+void encoder_read_POS(){
+	aState = MYgpioRead(PIN_ENCODER_A);
+
+	// If the state of A has changed
+	if (aState != aLastState && aState == HIGH){
+		encoder_action();
+	}
+
+	aLastState = aState;
+}
+
+/*******************************************************************************
+ * SWITCH FUNCTIONS
+ ******************************************************************************/
+
+void encoder_read_SW(){
+	cState = MYgpioRead(PIN_ENCODER_C);
+
+	if(cState == LOW){
+		if (cState != cLastState){
+			callback_SW_PRESS();
+		}
+	}
+	else{
+		if (cState != cLastState){
+			callback_SW_RELEASE();
+		}
+	}
+	cLastState = cState;
+}
+
+/*******************************************************************************
+ * GETTERS AND SETTERS
+ ******************************************************************************/
 
 int encoder_get_counter(){
 	return counter;
@@ -35,24 +102,10 @@ void encoder_set_callback_CCW(void (*userCallback_CCW)(void)){
 	callback_CCW = userCallback_CCW;
 }
 
-void encoder_set_callback_SW(void (*userCallback_SW)(void)){
-	callback_SW = userCallback_SW;
+void encoder_set_callback_SW_PRESS(void (*userCallback_SW_PRESS)(void)){
+	callback_SW_PRESS = userCallback_SW_PRESS;
 }
 
-void encoder_action(){
-
-	// If B is HIGH -> CW
-	if(gpioRead(PIN_ENCODER_B)){
-		counter ++;
-		callback_CW();
-	}
-	// If not -> CCW
-	else{
-		counter --;
-		callback_CCW();
-	}
-}
-
-void encoder_action_SW(){
-	callback_SW();
+void encoder_set_callback_SW_RELEASE(void (*userCallback_SW_RELEASE)(void)){
+	callback_SW_RELEASE = userCallback_SW_RELEASE;
 }
